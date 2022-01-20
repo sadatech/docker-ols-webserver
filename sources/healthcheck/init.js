c7c15fdae85c99f5715b7bf10304bbc0b9b98731
Andika Muhammad Cahya <andkmc99@gmail.com>
2022-01-20 15:54:08 +0700

    update healthcheck

--- sources/healthcheck/init.js
+++ sources/healthcheck/init.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/healthcheck-monitor", (req, res) => {
   res.send({ success: true, message: "It is working" });
 });
+app.get("/", (req, res) => {
+  // consider that this route crashes the entire application 
+});
 const PORT = process.env.PORT || 65530;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
